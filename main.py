0bc02e16648264e6c0fb7d9e57941407ea5c3e2f
Islom <islom.din.nur@gmail.com>
2020-01-26 23:26:58 +0500

    Алгоритм разложения числа на множители

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-	print(is_simple_number(5))
+	x = 996
+	print("Проверка простоты числа",x,":",is_simple_number(x))
+	print("Расклад числа",x,"на множители :")
+	factorize_number(x)
 
 def is_simple_number(x):
 	""" Метод грубой силы.
@@ -14,6 +17,19 @@ def is_simple_number(x):
 			return False
 		divisor += 1
 	return True
+
+def factorize_number(x):
+	""" Раскладывает число на множители.
+		Выводит на экран.
+		х - целое положительное число.
+	"""
+	divisor = 2
+	while x > 1:
+		if x%divisor == 0:
+			print(divisor)
+			x //= divisor
+		else:
+			divisor += 1
 
 if __name__ == "__main__":
 	main()
