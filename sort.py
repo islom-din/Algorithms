5b7081ed97afef0e69166b0d34bbe00f62a91385
Islom <islom.din.nur@gmail.com>
2020-01-28 23:50:58 +0500

    Некоторые улучшения

--- sort.py
+++ sort.py
@@ -1,30 +1,22 @@
+#---------------------------------------------
+#Квадратичные сортировки со сложностью O(n^2).
+#---------------------------------------------
 def insert_sort(A:list):
-	"""Сортировка вставками.
-	   Сложность: O(n^2).
-	   A - исходный неотсортированный массив.
-	"""
-	N = 0
-	for elem in A:
-		N += 1
+	"""Сортировка вставками."""
+	N = len(A)
 	length_of_sort_part = 0
 	while A[length_of_sort_part] < A[length_of_sort_part + 1]:
 		length_of_sort_part += 1
 	for i in range(length_of_sort_part + 1, N):
 		j = i
-		while A[j] < A[j-1]:
+		while A[j] < A[j-1] and j > 1:
 			A[j],A[j-1] = A[j-1],A[j]
-			if j > 1:
-				j -= 1
+			j -= 1
 	return A
 
 def choice_sort(A:list):
-	"""Сортировка выбором.
-	   Сложность: O(n^2).
-	   A - исходный неотсортированный массив.
-	"""
-	N = 0
-	for elem in A:
-		N += 1
+	"""Сортировка выбором."""
+	N = len(A)
 	for i in range(0,N):
 		Min = A[i]
 		pos = i
