e68c68a1771f515c15cbea4319c6c156c8fcfc8d
Islom <islom.din.nur@gmail.com>
2020-01-28 23:58:10 +0500

    Алгоритм сортировки пузырьком

--- sort.py
+++ sort.py
@@ -26,3 +26,11 @@ def choice_sort(A:list):
 				pos = j
 		A[i],A[pos] = A[pos],A[i]
 	return A
+
+def bubble_sort(A:list):
+	"""Сортировка пузырьком"""
+	N = len(A)
+	for bypass in range(1,N):
+		for k in range(0, N-bypass):
+			if A[k] > A[k+1]:
+				A[k], A[k+1] = A[k+1], A[k]
