dd84cf8839251510ef6b83f24f342ea38e56fd87
Islom <islom.din.nur@gmail.com>
2020-02-15 23:41:00 +0500

    Сортировка Тони Хоара

--- sort.py
+++ sort.py
@@ -77,4 +77,26 @@ def merge(A:list, B:list):
 		n += 1
 	return C
 
+def hoar_sort():
+	"""Сортировка Тони Хоара(Quick Sort)
+	"""
+	if len(A) <= 1
+		return
+	L = [] 
+	M = [] 
+	R = []
+	barrier = A[0]
+	for x in A:
+		if x < barrier:
+			L.append(x)
+		elif x == barrier:
+			M.append(x)
+		else:
+			R.append(x)
+	hoar_sort(L)
+	hoar_sort(R)
+	k = 0
+	for x in L+M+R:
+		A[k] = x
+		k += 1
 
