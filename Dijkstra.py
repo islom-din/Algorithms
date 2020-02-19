92d53ea2652b362c06b03257e65aa7a291fdc03d
Islom <islom.din.nur@gmail.com>
2020-02-19 21:36:51 +0500

    Алгоритм Дейкстры готов

--- Dijkstra.py
+++ Dijkstra.py
@@ -4,14 +4,27 @@
 """
 def main():
 	G = read_graph()
-	# start = input("С какой вершины начать?")
-	# while start not in G:
-	# 	start = input("Такой вершины в графе нет. С какой вершины начать?")
-	# shortest_distances = dijkstra(G,start)
-	# finish = input("До какой вершины?")
-	# while start not in G:
-	# 	start = input("Такой вершины в графе нет. До какой вершины?")
-	# shortest_path = reveal_shortes_path(start,finish,shortest_distances)
+	start = input("С какой вершины начать?")
+	while start not in G:
+		start = input("Такой вершины в графе нет. С какой вершины начать?")
+	shortest_distances = dijkstra(G,start)
+	finish = input("До какой вершины?")
+	while start not in G:
+		start = input("Такой вершины в графе нет. До какой вершины?")
+	shortest_path = reveal_shortes_path(G,start,finish,shortest_distances)
+
+
+def dijkstra(G, start):
+	Q = deque()
+	S = {}
+	S[start] = 0
+	Q.push(start)
+	while Q:
+		v = Q.pop()
+		for u in G[v]:
+			if u not in S or S[v] + G[v][u] < S[u]:
+				S[u] = S[v] + G[v][u]
+				Q.push(u)
 
 
 
@@ -24,7 +37,6 @@ def read_graph():
 		weight = float(weight)
 		add_edge(G, a, b, weight)
 		add_edge(G, b, a, weight)
-	print(G)
 	return G
 
 #Добавление ребра в исходный граф
